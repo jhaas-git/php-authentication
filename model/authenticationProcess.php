f3c9b88719c3a04e9fed4c9f1d34aa58148620d4
Joey <joeyhaas.git@gmail.com>
2022-01-28 16:45:46 +0100

    Account registration.
    Account creation system that checks for empty fields, correct password confirmation and already existing accounts.

--- model/authenticationProcess.php
+++ model/authenticationProcess.php
@@ -1,6 +1,50 @@
 <?php 
     function registerAccount() {
+        require 'model/pdo/config.php';
 
+        // Make sure the post values aren't empty.
+        if (!empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['mail_address']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
+            // When post values are filled, check if the password is equal to the password confirmation.
+            if (($_POST['password']) == ($_POST['confirm_password'])) {
+                // When the password and confirmation match, assign the post values to a variable.
+                $first = $_POST['first_name'];
+                $last = $_POST['last_name'];
+                $mail = $_POST['mail_address'];
+                $pass = $_POST['password']; 
+                $hash = hash('sha256', $pass); // Password will be hashed.
+
+                // Prevent duplicates by checking if the user already exists.
+                $fetchExistingAccount = 'SELECT * FROM account WHERE sMailAddress = :post_mail';
+                $stmt = $pdo->prepare($fetchExistingAccount);
+                $stmt->execute([
+                    ':post_mail' => $mail
+                ]);
+                // Assigning rowCount result to a readable variable.
+                $existingAccount = $stmt->rowCount();
+
+                if ($existingAccount > 0) { 
+                    // If more than zero results are found,
+                    // redirect to register page. error message sent through the GET.
+                    header('location: template/account.php?register=alreadyExists');
+                } elseif ($existingAccount == 0) {
+                    // When no results are found, insert post values into database.
+                    $insertAccount = 'INSERT INTO account (sFirstname, sLastname, sMailaddress, sPassword)
+                    VALUES (:post_first, :post_last, :post_mail, :post_pass)';
+                    $stmt = $pdo->prepare($insertAccount);
+                    $stmt->execute([
+                        ':post_first' => $first,
+                        ':post_last' => $last,
+                        ':post_mail' => $mail,
+                        ':post_pass' => $hash
+                    ]);
+                    header('location: template/account.php?registrationSuccessfull'); // Redirect to login page, success message sent through the GET.
+                }
+            } else {
+                header('location: template/account.php?register=matchingPassword'); // Redirect to register page, error message sent through the GET.
+            }
+        } else {
+            header('location: template/account.php?register=missingFields'); // Redirect to register page, error message sent through the GET.
+        }
     }
 
     function authenticateAccount() {
